9203c864f4510de52b56a5cc4251bee10f02df8e
dayniel-caadiang <dayniel.caadiang@betterask.erni>
2025-12-09 14:47:45 +0800

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,7 @@ import '../models/user_data.dart';
 import 'registration_page1.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
